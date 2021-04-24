5df5ffa9ea2a40f99f673e4130fe2fbf32fcfd22
adriraxor <figueres.adrien@gmail.com>
2021-04-24 10:41:42 +0200

    Mise à jour : - Récupération du nombre de produits existant ! - Récupération de la valeur du stock (Produits) - Récupération du nombre de clients inscrits - Mise à jour du design et corrections de bugs
    Prochainement :
    - Correction des problèmes visuels connues
    - Ajout de fonctionnalités
    - Amélioration ergonomie et design
    - Authentification sécurisée pour les administrateurs

--- app/src/main/java/com/example/bts_apisymfony/Clients.java
+++ app/src/main/java/com/example/bts_apisymfony/Clients.java
@@ -1,7 +1,9 @@
 package com.example.bts_apisymfony;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -83,7 +85,7 @@ public class Clients extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_clients, container, false);
         final Button btnClient = root.findViewById(R.id.btn_getAllClients);
-
+        final Button btn_getNbClients = root.findViewById(R.id.btn_getNbClients);
 
         btnClient.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("ShowToast")
@@ -92,6 +94,57 @@ public class Clients extends Fragment {
                 Toast.makeText(v.getContext(), "http://192.168.187.136:8082/bts-app/public/ApiAllProducts", Toast.LENGTH_SHORT).show();
                 Intent intentActivityAllClients = new Intent(v.getContext(), GetAllClients_Activity.class);
                 startActivity(intentActivityAllClients);
+            }
+        });
+
+        btn_getNbClients.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ShowToast")
+            @Override
+            public void onClick(View v) {
+                //le contexte est l'instance (this) de l'activity APIActivity
+                RequestQueue queue = Volley.newRequestQueue(v.getContext());
+
+                //url du service à consommer
+                String url = "http://192.168.187.138:8082/bts-app/public/ApiNbClientsInscrits";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                //parcours des Clients retournés
+                                ArrayList<String> leNbClient = new ArrayList<String>();
+                                String nbCVar = null;
+                                JSONArray jsonArray = null;
+                                try {
+                                    jsonArray = new JSONArray(response);
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject item = jsonArray.getJSONObject(i);
+                                        String nbC = item.getString("nb_client");
+                                        nbCVar = nbC;
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                                builder.setTitle("Nombre de clients inscrits");
+                                builder.setMessage(nbCVar);
+                                builder.setCancelable(true);
+                                builder.setNegativeButton("Fermer", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //Fermeture de la boîte de dialogue
+                                    }
+                                });
+                                builder.create();
+                                builder.show();
+                            }
+                        }, new Response.ErrorListener() {        // CAS d’ERREUR
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(v.getContext(), "[NBPRODUITS] Une erreur à été détecté !", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                queue.add(stringRequest);
             }
         });
 
--- app/src/main/java/com/example/bts_apisymfony/Produits.java
+++ app/src/main/java/com/example/bts_apisymfony/Produits.java
@@ -1,17 +1,38 @@
 package com.example.bts_apisymfony;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -67,11 +88,15 @@ public class Produits extends Fragment {
         final Button btn_allTendanceProducts = root.findViewById(R.id.btn_getTendance);
         final Button btn_allRecentProducts = root.findViewById(R.id.btn_getRecent);
         final Button btn_allComingSoon = root.findViewById(R.id.btn_getComingSoon);
+        final Button btn_nbProducts = root.findViewById(R.id.btn_getNbProducts);
+        final Button btn_valueStock = root.findViewById(R.id.btn_getMontantStock);
+
 
         btn_allProducts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "http://192.168.187.138:8082/bts-app/public/ApiAllProducts", Toast.LENGTH_SHORT).show();
+                Toast allPrd = Toast.makeText(v.getContext(), "http://192.168.187.138:8082/bts-app/public/ApiAllProducts", Toast.LENGTH_SHORT);
+                allPrd.show();
                 Intent intentActivityAllProducts = new Intent(v.getContext(), GetAllProducts_Activity.class);
                 startActivity(intentActivityAllProducts);
             }
@@ -80,7 +105,8 @@ public class Produits extends Fragment {
         btn_allTendanceProducts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "http://192.168.187.138:8082/bts-app/public/ApiAllTendanceProducts", Toast.LENGTH_SHORT).show();
+                Toast tendancePrd = Toast.makeText(v.getContext(), "http://192.168.187.138:8082/bts-app/public/ApiAllTendanceProducts", Toast.LENGTH_SHORT);
+                tendancePrd.show();
                 Intent intentActivityAllTendanceProducts = new Intent(v.getContext(), GetAllTendance_Activity.class);
                 startActivity(intentActivityAllTendanceProducts);
             }
@@ -89,7 +115,8 @@ public class Produits extends Fragment {
         btn_allRecentProducts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "http://192.168.187.138:8082/bts-app/public/ApiAllRecentProducts", Toast.LENGTH_SHORT).show();
+                Toast recentPrd = Toast.makeText(v.getContext(), "http://192.168.187.138:8082/bts-app/public/ApiAllRecentProducts", Toast.LENGTH_SHORT);
+                recentPrd.show();
                 Intent intentActivityAllRecentProducts = new Intent(v.getContext(), GetAllRecentProducts_Activity.class);
                 startActivity(intentActivityAllRecentProducts);
             }
@@ -98,16 +125,111 @@ public class Produits extends Fragment {
         btn_allComingSoon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "http://192.168.187.138:8082/bts-app/public/ApiAllCommingSoonProducts", Toast.LENGTH_SHORT).show();
+                Toast comingSoonPrd = Toast.makeText(v.getContext(), "http://192.168.187.138:8082/bts-app/public/ApiAllCommingSoonProducts", Toast.LENGTH_SHORT);
+                comingSoonPrd.show();
                 Intent intentActivityAllComingSoon = new Intent(v.getContext(), GetAllComingSoonProducts_Activity.class);
                 startActivity(intentActivityAllComingSoon);
             }
         });
 
+        btn_nbProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //le contexte est l'instance (this) de l'activity APIActivity
+                RequestQueue queue = Volley.newRequestQueue(v.getContext());
 
+                //url du service à consommer
+                String url = "http://192.168.187.138:8082/bts-app/public/ApiCountTotalProducts";
 
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                //parcours des Clients retournés
+                                ArrayList<String> leTotalPrd = new ArrayList<String>();
+                                String nbVar = null;
+                                JSONArray jsonArray = null;
+                                try {
+                                    jsonArray = new JSONArray(response);
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject item = jsonArray.getJSONObject(i);
+                                        String nb = item.getString("Nombre_de_produit");
+                                        nbVar = nb;
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                                builder.setTitle("Nombre de produit total");
+                                builder.setMessage(nbVar);
+                                builder.setCancelable(true);
+                                builder.setNegativeButton("Fermer", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //Fermeture de la boîte de dialogue
+                                    }
+                                });
+                                builder.create();
+                                builder.show();
+                            }
+                        }, new Response.ErrorListener() {        // CAS d’ERREUR
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(v.getContext(), "[NBPRODUITS] Une erreur à été détecté !", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                queue.add(stringRequest);
+            }
+        });
 
-        // Inflate the layout for this fragment
+        btn_valueStock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //le contexte est l'instance (this) de l'activity APIActivity
+                RequestQueue queue = Volley.newRequestQueue(v.getContext());
+
+                //url du service à consommer
+                String url = "http://192.168.187.138:8082/bts-app/public/ApiValueStockProducts";
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                //parcours
+                                String totalVar = null;
+                                JSONArray jsonArray = null;
+                                try {
+                                    jsonArray = new JSONArray(response);
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject item = jsonArray.getJSONObject(i);
+                                        String valeur_stock = item.getString("valeur_stock");
+                                        totalVar = valeur_stock;
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                                builder.setTitle("Valeur du stock");
+                                builder.setMessage(totalVar + "€");
+                                builder.setCancelable(true);
+                                builder.setNegativeButton("Fermer", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //Fermeture de la boîte de dialogue
+                                    }
+                                });
+                                builder.create();
+                                builder.show();
+                            }
+                        }, new Response.ErrorListener() {        // CAS d’ERREUR
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(v.getContext(), "[NBPRODUITS] Une erreur à été détecté !", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                queue.add(stringRequest);
+            }
+        });
         return root;
     }
 }
